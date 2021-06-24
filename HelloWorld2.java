7a56a158d0c8568f4cbbfcd6ebbe7e5d73ae6d08
Mandla Thwethwa <mandlatwetwa@gmail.com>
2021-06-24 14:18:47 +0200

    HelloWorld2.java

---
+++ HelloWorld2.java
@@ -0,0 +1,15 @@
+package helloWorld2;
+
+public class HelloWorld2 {
+	
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			System.out.print("Git is Awesome!");
+		}
+
+	}
+
+
+
+
+
